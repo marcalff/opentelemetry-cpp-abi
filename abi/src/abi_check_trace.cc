71d74581adb9875caf0fd9a66ac0f4312886ed31
Marc Alff <marc.alff@oracle.com>
2024-11-26 15:44:47 +0100

    Update to opentelemetry-cpp 1.18.0, expand trace test case.

--- abi/src/abi_check_trace.cc
+++ abi/src/abi_check_trace.cc
@@ -30,5 +30,15 @@ void do_abi_check_trace() {
   auto p = opentelemetry::trace::Provider::GetTracerProvider();
   auto tracer = p->GetTracer("abi");
   auto span = tracer->StartSpan("abi");
+  span->AddEvent("abi", {{"a", 1}, {"b", "2"}, {"c", 3.0}});
   span->End();
+
+  std::map<std::string, std::string> attrs = {{"a", "3"}};
+  std::vector<std::pair<opentelemetry::trace::SpanContext, std::map<std::string, std::string>>> links = {
+      {opentelemetry::trace::SpanContext(false, false), attrs}};
+  auto s1 = tracer->StartSpan("abc", attrs, links);
+
+  auto s2 =
+      tracer->StartSpan("efg", {{"a", 3}}, {{opentelemetry::trace::SpanContext(false, false), {{"b", 4}}}});
+
 }
